4b2171312c9fe3653d81caa4dde470c2f939d53b
Sohan Patel <sohan.dynamic.methods@gmail.com>
2017-07-11 19:01:43 +0530

    1st test

---
+++ ip.php
@@ -0,0 +1,7 @@
+<?php
+
+//$content = @file_get_contents("lbtext.log");
+//$content=file_get_contents("lbtext.log", $use_include_path=false, $context=null, 0, 500);
+$timestamp = date("YmdHis");
+print_r($timestamp);
+?>
